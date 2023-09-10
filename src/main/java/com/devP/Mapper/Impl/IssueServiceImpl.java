1f37e5464cf6765fe46ba78467d445ee20dd0975
Limdongdang <dio8620@naver.com>
2023-09-11 03:40:12 +0900

    이슈 css, 프론트 작업 완료

--- src/main/java/com/devP/Mapper/Impl/IssueServiceImpl.java
+++ src/main/java/com/devP/Mapper/Impl/IssueServiceImpl.java
@@ -8,11 +8,13 @@ import com.devP.Service.MailService;
 import com.devP.Service.UserService;
 import com.devP.VO.IssueVO;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -22,31 +24,31 @@ public class IssueServiceImpl implements IssueService {
 	private IssueDAOMybatis issueDAO;
 	@Autowired
 	private HttpSession session;
-	
-    private String from = "daggggg2@naver.com";
+
+	private String from = "daggggg2@naver.com";
 
 	@Autowired
-    private MailService mailService;
+	private MailService mailService;
 	@Autowired
 	private CommentService commentService;
 	@Autowired
 	private UserService userService;
-	
-	
+
 	@Override
-	public int insertIssue(IssueVO issue){
+	public int insertIssue(IssueVO issue) {
 		String emails = issue.getSendingEmail();
-		//세션 아이디 정보 등록
+		// 세션 아이디 정보 등록
 		issue.setUserId(session.getAttribute("id").toString());
-		//이슈 상태 초기 설정
+		// 이슈 상태 초기 설정
+		System.out.println(issue.getIssueId());
 		issue.setStatus("대기");
-		
+
 		// 구분자를 쉼표(,)로 지정하여 문자열을 나누고, 이메일 주소들을 ArrayList에 저장
-        ArrayList<String> emailList = new ArrayList<>();
-        String[] emailArray = emails.split(",");
-        for (String email : emailArray) {
-            emailList.add(email);
-        }
+		ArrayList<String> emailList = new ArrayList<>();
+		String[] emailArray = emails.split(",");
+		for (String email : emailArray) {
+			emailList.add(email);
+		}
 		try {
 			mailService.sendMail(from, emailList, issue.getUserId() + "(이)가 이슈 알림을 보냈습니다", issue.getContent());
 		} catch (Exception e) {
@@ -54,19 +56,27 @@ public class IssueServiceImpl implements IssueService {
 		}
 		return issueDAO.insertIssue(issue);
 	}
-	
+
 	@Override
 	public int getIssuelist(int projectId, Model model) {
-		String[] statusArray = {"대기", "검토", "해결"};
+		String[] statusArray = { "대기", "검토", "해결" };
+		List<IssueVO> vo = issueDAO.getIssuelist(projectId);
+		/*
+		 * for (IssueVO issue : vo) {// 출력 형식 지정 SimpleDateFormat outputFormat = new
+		 * SimpleDateFormat("yyyy/MM/dd HH시 mm분"); String formattedDate =
+		 * outputFormat.format(issue.getDate()); issue.setDate(formattedDate); }
+		 */
 		model.addAttribute("issueList", issueDAO.getIssuelist(projectId));
 		model.addAttribute("statusarr", statusArray);
 		return 0;
 	}
+
 	@Override
 	public int deleteIssue(IssueVO issue) {
 		return issueDAO.deleteIssue(issue.getIssueId());
 	}
-	//이슈 상세 - 추가 작업 댓글 추가 해야 됨
+
+	// 이슈 상세 - 추가 작업 댓글 추가 해야 됨
 	@Override
 	public int getIssue(int issueId, Model model) {
 		try {
@@ -75,9 +85,9 @@ public class IssueServiceImpl implements IssueService {
 			if ("대기".equals(issue.getStatus().toString())) {
 				issue.setStatus("검토");
 			}
-			//이슈 조회수 올리기 -추가 작업 동일 세션 아이디 중복 조회 제한
+			// 이슈 조회수 올리기 -추가 작업 동일 세션 아이디 중복 조회 제한
 			issueDAO.countupIssue(issueId);
-			//이슈 상태 변경
+			// 이슈 상태 변경
 			issueDAO.changeIssueStatus(issue);
 			model.addAttribute("issue", issue);
 			model.addAttribute("commentList", commentService.getComment(issue.getIssueId()));
@@ -87,9 +97,10 @@ public class IssueServiceImpl implements IssueService {
 		}
 		return 0;
 	}
+
 	@Override
 	public int solveIssue(IssueVO issue) {
-		issue.setStatus("해결");	
+		issue.setStatus("해결");
 		try {
 			issueDAO.changeIssueStatus(issue);
 		} catch (Exception e) {
@@ -103,16 +114,16 @@ public class IssueServiceImpl implements IssueService {
 	public int modifyIssue(IssueVO issue) {
 		String emails = issue.getSendingEmail();
 		try {
-			//세션 아이디 정보 등록
+			// 세션 아이디 정보 등록
 			issue.setUserId(session.getAttribute("id").toString());
 			// 구분자를 쉼표(,)로 지정하여 문자열을 나누고, 이메일 주소들을 ArrayList에 저장
-	        ArrayList<String> emailList = new ArrayList<>();
-	        String[] emailArray = emails.split(",");
-	        for (String email : emailArray) {
-	            emailList.add(email);
-	        }
-	        //이메일 알림 전송
-	        mailService.sendMail(from, emailList, issue.getUserId() + "(이)가 이슈 수정 알림을 보냈습니다", issue.getContent());
+			ArrayList<String> emailList = new ArrayList<>();
+			String[] emailArray = emails.split(",");
+			for (String email : emailArray) {
+				emailList.add(email);
+			}
+			// 이메일 알림 전송
+			mailService.sendMail(from, emailList, issue.getUserId() + "(이)가 이슈 수정 알림을 보냈습니다", issue.getContent());
 			issueDAO.modifyIssue(issue);
 		} catch (Exception e) {
 			System.out.println(e);
---
+++ src/main/webapp/WEB-INF/views/issueWrite.jsp
@@ -0,0 +1,75 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page session="true" %>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<%@include file="sidebar.jsp"%>
+<head>
+    <link rel="stylesheet" href="/resources/css/issue.css?A">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+<%
+String username = (String) session.getAttribute("name");
+%>
+<div class="container">
+    <div class="issue-wrapper">
+		<div class="mt-5">
+	        <h1 class="mb-4">Moment 이슈 등록</h1>
+	        <form method="post" class="p-3" action="write.do" id="issue-form">
+            	 <input class = "issueInput" type = "hidden" name="projectId" value = "1"><br>
+            	 <input class = "issueInput" type = "hidden" name="taskId" value = "1"><br>
+	             <div class="mb-3 row">
+				    <label class="col-sm-2 col-form-label">프로젝트</label>
+				    <div class="col-sm-10">
+				      <input type="text" readonly class="form-control-plaintext" name="projectTitle" value="프로젝트">
+				    </div>
+				  </div>
+				  <div class="mb-3 row">
+				    <label class="col-sm-2 col-form-label">안건</label>
+				    <div class="col-sm-10">
+				      <select class="form-select" aria-label="Default select example" name="category">
+						  <option value="" selected>선택</option>
+						  <option value="논의">논의</option>
+						  <option value="개발">개발</option>
+						  <option value="제안">제안</option>
+						</select>
+				    </div>
+				  </div>
+				  <div class="mb-3 row">
+				    <label class="col-sm-2 col-form-label">작성자	</label>
+				    <div class="col-sm-10">
+				      <input type="text" readonly class="form-control-plaintext" name="writer" value="${ sessionScope.name }">
+				    </div>
+				  </div>
+				  <div class="mb-3 row">
+				    <label class="col-sm-2 col-form-label">제목</label>
+				    <div class="col-sm-10">
+				      <input type="text" class="form-control" name="title">
+				    </div>
+				  </div>
+				  <div class="mb-3 row">
+				    <label class="col-sm-2 col-form-label">알림</label>
+				    <div class="col-sm-10">
+				      <input type="text" class="form-control" name="sendingEmail">
+				    </div>
+				  </div>
+				  <div class="mb-3 row">
+				    <label class="col-sm-2 col-form-label">내용</label>
+				    <div class="col-sm-10">
+				      <textarea type="text" class="form-control" name="content" style="height : 15vh"></textarea>
+				    </div>
+				  </div>
+				  <div>
+		          	<button type="submit" class="btn btn-primary" style="float: right">등록</button>
+		          </div>
+	        </form>
+	    </div>
+	</div>
+</div>
+<script>
+	var selectBox = function(value){
+		}
+</script>
+</body>
+</html>
