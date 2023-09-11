9272afeef4239296a74f1dddda801a972dde70ba
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2023-09-11 18:57:09 +0900

    [멤버페이지]깃 오류 및 충동해결

--- src/main/java/com/devP/Mapper/Repository/ProjectDAOMybatis.java
+++ src/main/java/com/devP/Mapper/Repository/ProjectDAOMybatis.java
@@ -1,15 +1,2 @@
-package com.devP.Mapper.Repository;
-
-import com.devP.VO.ProjectVO;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ProjectDAOMybatis {
-
-    @Autowired
-    private SqlSessionTemplate mybatis;
-
-    public int insertProject(ProjectVO vo) {return mybatis.insert("ProjectDAO.insertProject", vo);}
+package com.devP.Mapper.Repository;public class ProjectDAOMyBatis {
 }
