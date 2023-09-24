1cc6a2b473574ac8b5d92e82ca57670e953c2559
marie-lee <mariesejinlee@gmail.com>
2023-09-24 22:46:58 +0900

    clear git cache

--- target/view-1.0.0-BUILD-SNAPSHOT/resources/js/checkAuthKey.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('checkBtn').addEventListener('click', function(e){
-    var authKey = [[%{authKey}]];
-    var inputKey = document.getElementById("checkingCode");
-    if(authKey == null){
-        alert("");
-    }
-});
