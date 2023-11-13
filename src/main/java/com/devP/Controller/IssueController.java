1ceb143a13f8b5d15ad836b488b6f50368d67657
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2023-11-13 16:31:28 +0900

    이슈메일전송오류 및 프로젝트리스트 진행률 수정

--- src/main/java/com/devP/Controller/IssueController.java
+++ src/main/java/com/devP/Controller/IssueController.java
@@ -59,6 +59,7 @@ public class IssueController {
 		model.addAttribute("menuId", "issueMenu");
 		if(issue.getTaskId() == 0){issue.setTaskId(null);}
 		issueService.insertIssue(issue);
+
 		return "redirect:/issue/list.do?projectId=" + issue.getProjectId();
     }
 
